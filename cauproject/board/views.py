73a5f31073bec0ce6b66974c873a95cf7ad0794f
LDongGyu <ehdrbchd@naver.com>
2019-02-27 17:58:31 +0900

    notice 모델 추가

--- cauproject/board/views.py
+++ cauproject/board/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.core.paginator import Paginator
-from .models import Post
+from .models import Post,Notice
 
 # Create your views here.
 def home(request):
@@ -11,15 +11,20 @@ def about(request):
   return render(request,'about.html')
 
 def free_board(request):
-  return render(request,'free_board.html')
-
-def inquire(request):
-  return render(request,'inquire.html')
-
-def notice(request):
   posts = Post.objects
   posts_list = Post.objects.all()
   paginator = Paginator(posts_list,5)
   page = request.GET.get('page')
   pages = paginator.get_page(page)
-  return render(request, 'notice.html',{'posts':posts, 'pages':pages})
+  return render(request,'free_board.html',{'posts':posts,'pages':pages})
+
+def inquire(request):
+  return render(request,'inquire.html')
+
+def notice(request):
+  notices = Notice.objects
+  notices_list = Notice.objects.all()
+  paginator = Paginator(notices_list,5)
+  page = request.GET.get('page')
+  pages = paginator.get_page(page)
+  return render(request, 'notice.html',{'notices':notices, 'pages':pages})
