eccdefe768ced8748861f8a4095c2689b26a6f01
LDongGyu <ehdrbchd@naver.com>
2019-03-07 10:09:22 +0900

    settings.py allowed_host *

--- cauproject/settings.py
+++ cauproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','5(j&l)yscgmoi#*0l5-=_j482^w0w_+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG',True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
